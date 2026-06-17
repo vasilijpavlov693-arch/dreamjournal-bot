b99fa150314e0f69159a6e6642e33393680eb635
vasilijpavlov693-arch <vasilijpavlov693@gmail.com>
2026-06-17 20:44:02 +0300

    Update bot.py
    фикс ошибки доступа к нейросети на huggingface

--- bot.py
+++ bot.py
@@ -124,7 +124,7 @@ async def generate_image(prompt: str) -> BytesIO | None:
 
     try:
         # 1. Формируем запрос
-        API_URL = "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-dev"
+        API_URL = "https://huggingface.co/api/inference-proxy/models/black-forest-labs/FLUX.1-dev"
         headers = {"Authorization": f"Bearer {HUGGINGFACE_TOKEN}"}
         
         # Используем короткий промпт для ускорения
